f3ecd5e53ca4e5e462c9af053c293da2a236cd38
toopeachok <47483838+toopeachok@users.noreply.github.com>
2020-03-17 02:26:12 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,129 @@
+// Start menu and start button
+document.getElementById("start-button").onclick = function () {
+  document.getElementById("start-menu").style.display = "none";
+}
+
+//Color picker
+var colorPicker = document.getElementById("color-picker");
+var colorPickerWrapper = document.getElementById("color-picker-wrapper");
+var lineColor = "#000000";
+
+colorPicker.onchange = function() {
+  lineColor = "" + colorPicker.value;
+  colorPickerWrapper.style.backgroundColor = lineColor;
+}
+
+//Line thickness
+var lineThickness = 1;
+var lineThicknessInput = document.getElementById("line-thickness");
+lineThicknessInput.onchange = function () {
+  lineThickness = lineThicknessInput.value;
+}
+
+//Draw type
+var drawType = "line";
+var lineInput = document.getElementById("line");
+lineInput.onclick = function() {
+  drawType = "line";
+  document.getElementById("active-line").style.opacity = 1;
+  document.getElementById("active-fillRectangle").style.opacity = 0;
+  document.getElementById("active-strokeRectangle").style.opacity = 0;
+}
+
+var fillRecInput = document.getElementById("fillRectangle");
+fillRecInput.onclick = function() {
+  drawType = "fillRectangle";
+  document.getElementById("active-line").style.opacity = 0;
+  document.getElementById("active-fillRectangle").style.opacity = 1;
+  document.getElementById("active-strokeRectangle").style.opacity = 0;
+}
+
+var strokeRecInput = document.getElementById("strokeRectangle");
+strokeRecInput.onclick = function() {
+  drawType = "strokeRectangle";
+  document.getElementById("active-line").style.opacity = 0;
+  document.getElementById("active-fillRectangle").style.opacity = 0;
+  document.getElementById("active-strokeRectangle").style.opacity = 1;
+}
+
+//Canvas
+var mainCanvas = document.getElementById("canvas-main");
+var bufferCanvas = document.getElementById("canvas-buffer");
+
+if (mainCanvas.getContext) {
+  var isDrawing = false; // When true, moving the mouse draws on the canvas
+  var x = 0;
+  var y = 0;
+  var mainContext = mainCanvas.getContext("2d");
+  var bufferContext = bufferCanvas.getContext("2d");
+  var rect = mainCanvas.getBoundingClientRect(); // The x and y offset of the canvas from the edge of the page
+
+  // Add the event listeners for mousedown, mousemove, and mouseup
+  bufferCanvas.addEventListener('mousedown', e => {
+  x = e.clientX - rect.left;
+  y = e.clientY - rect.top;
+  isDrawing = true;
+  mainContext.drawImage(bufferCanvas, 0, 0);
+  });
+
+  bufferCanvas.addEventListener('mousemove', e => {
+    if (isDrawing === true) {
+      if (drawType === "line") {
+        drawLine(bufferContext, x, y, e.clientX - rect.left, e.clientY - rect.top, lineThickness, lineColor);
+        x = e.clientX - rect.left;
+        y = e.clientY - rect.top;
+      }
+
+      if (drawType === "fillRectangle") {
+        bufferContext.clearRect(0, 0, 1200, 600);
+        drawFillRect(bufferContext, x, y, e.clientX - rect.left - x, e.clientY - rect.top - y, lineColor);
+      }
+
+      if (drawType === "strokeRectangle") {
+        bufferContext.clearRect(0, 0, 1200, 600);
+        drawStrokeRect(bufferContext, x, y, e.clientX - rect.left - x, e.clientY - rect.top - y, lineColor);
+      }
+    }
+  });
+  
+  window.addEventListener('mouseup', e => {
+    if (isDrawing === true) {
+      if (drawType === "line") {
+        drawLine(mainContext, x, y, e.clientX - rect.left, e.clientY - rect.top, lineThickness, lineColor);
+      } else {
+        mainContext.drawImage(bufferCanvas, 0, 0);
+      }
+      // if (drawType === "fillRectangle") {
+      //   drawFillRect(mainContext, x, y, e.clientX - rect.left - x, e.clientY - rect.top - y, lineColor);
+      // }
+      // if (drawType === "strokeRectangle") {
+      //   drawStrokeRect(mainContext, x, y, e.clientX - rect.left - x, e.clientY - rect.top - y, lineColor);
+      // }
+      x = 0;
+      y = 0;
+      isDrawing = false;
+    }
+  });
+  
+  function drawLine(context, x1, y1, x2, y2, lineThickness, lineColor) {
+    context.beginPath();
+    context.strokeStyle = lineColor;
+    context.lineWidth = lineThickness;
+    context.lineCap = "round";
+    context.moveTo(x1, y1);
+    context.lineTo(x2, y2);
+    context.stroke();
+    context.closePath();
+  }
+
+  function drawFillRect(context, x1, y1, widthRect, heightRect, color) {
+    context.fillStyle = color;
+    context.fillRect(x1, y1, widthRect, heightRect);
+  }
+  
+  function drawStrokeRect(context, x1, y1, widthRect, heightRect, color) {
+    context.strokeStyle = color;
+    context.lineWidth = lineThickness;
+    context.strokeRect(x1, y1, widthRect, heightRect);
+  }
+}
