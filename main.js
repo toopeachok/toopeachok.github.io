24ad2ff2026de0f31620000d019d3fd8726955a4
toopeachok <47483838+toopeachok@users.noreply.github.com>
2020-03-19 15:06:14 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -52,6 +52,8 @@ lineInput.onclick = function() {
   document.getElementById("active-line").style.opacity = 1;
   document.getElementById("active-fillRectangle").style.opacity = 0;
   document.getElementById("active-strokeRectangle").style.opacity = 0;
+  document.getElementById("active-strokeCircle").style.opacity = 0;
+  document.getElementById("active-strokeStar").style.opacity = 0;
 }
 
 var fillRecInput = document.getElementById("fillRectangle");
@@ -60,6 +62,8 @@ fillRecInput.onclick = function() {
   document.getElementById("active-line").style.opacity = 0;
   document.getElementById("active-fillRectangle").style.opacity = 1;
   document.getElementById("active-strokeRectangle").style.opacity = 0;
+  document.getElementById("active-strokeCircle").style.opacity = 0;
+  document.getElementById("active-strokeStar").style.opacity = 0;
 }
 
 var strokeRecInput = document.getElementById("strokeRectangle");
@@ -68,6 +72,28 @@ strokeRecInput.onclick = function() {
   document.getElementById("active-line").style.opacity = 0;
   document.getElementById("active-fillRectangle").style.opacity = 0;
   document.getElementById("active-strokeRectangle").style.opacity = 1;
+  document.getElementById("active-strokeCircle").style.opacity = 0;
+  document.getElementById("active-strokeStar").style.opacity = 0;
+}
+
+var strokeRecInput = document.getElementById("strokeCircle");
+strokeRecInput.onclick = function() {
+  drawType = "strokeCircle";
+  document.getElementById("active-line").style.opacity = 0;
+  document.getElementById("active-fillRectangle").style.opacity = 0;
+  document.getElementById("active-strokeRectangle").style.opacity = 0;
+  document.getElementById("active-strokeCircle").style.opacity = 1;
+  document.getElementById("active-strokeStar").style.opacity = 0;
+}
+
+var strokeRecInput = document.getElementById("strokeStar");
+strokeRecInput.onclick = function() {
+  drawType = "strokeStar";
+  document.getElementById("active-line").style.opacity = 0;
+  document.getElementById("active-fillRectangle").style.opacity = 0;
+  document.getElementById("active-strokeRectangle").style.opacity = 0;
+  document.getElementById("active-strokeCircle").style.opacity = 0;
+  document.getElementById("active-strokeStar").style.opacity = 1;
 }
 
 //Canvas
@@ -107,6 +133,16 @@ if (mainCanvas.getContext) {
         bufferContext.clearRect(0, 0, 1200, 600);
         drawStrokeRect(bufferContext, x, y, event.offsetX - x, event.offsetY - y, lineColor);
       }
+
+      if (drawType === "strokeCircle") {
+        bufferContext.clearRect(0, 0, 1200, 600);
+        drawStrokeRCircle(bufferContext, x, y, event.offsetX - x, event.offsetY - y, lineColor);
+      }
+
+      if (drawType === "strokeStar") {
+        bufferContext.clearRect(0, 0, 1200, 600);
+        drawStrokeStar(bufferContext, x, y, event.offsetX - x, event.offsetY - y, lineColor);
+      }
     }
   }
 
@@ -144,10 +180,51 @@ if (mainCanvas.getContext) {
     context.lineWidth = lineThickness;
     context.strokeRect(x1, y1, widthRect, heightRect);
   }
-}
+
+  function drawStrokeRCircle(context, x1, y1, x2, y2, color) {
+    var radius = Math.sqrt( Math.pow(x2, 2) + Math.pow(y2, 2) );
+    context.strokeStyle = color;
+    context.lineWidth = lineThickness;
+    context.beginPath();
+    context.arc(x1, y1, radius, 0, 2 * Math.PI);
+    context.stroke();
+    context.closePath();
+  }
+
+  function drawStrokeStar(context, x1, y1, x2, y2, color) {
+      var spikes = 5;
+      var rot = Math.PI / 2 * 3;
+      var x = x1;
+      var y = y1;
+      var step = Math.PI / spikes;
+      var outerRadius = Math.sqrt( Math.pow(x2, 2) + Math.pow(y2, 2) );
+      var innerRadius = outerRadius / 3;
+
+      context.strokeSyle = "#000";
+      context.beginPath();
+      context.moveTo(x1, y1 - outerRadius)
+      for (i = 0; i < spikes; i++) {
+          x = x1 + Math.cos(rot) * outerRadius;
+          y = y1 + Math.sin(rot) * outerRadius;
+          context.lineTo(x, y)
+          rot += step
+
+          x = x1 + Math.cos(rot) * innerRadius;
+          y = y1 + Math.sin(rot) * innerRadius;
+          context.lineTo(x, y)
+          rot += step
+      }
+      context.lineTo(x1, y1 - outerRadius)
+      context.closePath();
+      context.lineWidth = lineThickness;
+      context.strokeStyle=color;
+      context.stroke();
+  }
 
   var clearRectInput = document.getElementById("clearRectangle");
   clearRectInput.onclick = function() {
     mainContext.clearRect(0, 0, 1200, 600);
     bufferContext.clearRect(0, 0, 1200, 600);
   }
+}
+
